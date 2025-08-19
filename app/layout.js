15bc7e8b6e3d5fde880655d5bdfc9455a5f928f9
Anil Tigga <anil.anil300@gmail.com>
2025-08-20 01:09:16 +0530

    fix

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SideAndHeader from "@/components/sideAndHeader";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SideAndHeader>
-          {children}
-        </SideAndHeader>
+        <Suspense>
+          <SideAndHeader>
+            {children}
+          </SideAndHeader>
+        </Suspense>
       </body>
     </html>
   );
